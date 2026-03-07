e13525c15c8754cfaee348135b8ea192ee001cf7
Cory Forseth <forsethc@gmail.com>
2026-03-06 19:55:30 -0600

    purposely broken test

--- tests/unit/test_avr_connection_factory.py
+++ tests/unit/test_avr_connection_factory.py
@@ -1,6 +1,7 @@
 """Unit tests: create_avr_connection factory (avr_connection)."""
 from avr_connection import AVRConnection, VirtualAVRConnection, create_avr_connection
 from avr_state import AVRState
+import pytest
 
 
 def _make_callbacks():
@@ -18,6 +19,7 @@ def _make_callbacks():
 
 def test_create_avr_connection_returns_physical_when_avr_host_set():
     """When avr_host is set, returns AVRConnection."""
+    pytest.fail("test branch protection")
     state, on_resp, on_disc, _ = _make_callbacks()
     config = {"avr_host": "192.168.1.100", "avr_port": 23}
     import logging
